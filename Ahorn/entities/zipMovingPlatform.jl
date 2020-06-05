7c0ccc83b12cf19e538f4b5414465d9bef1deb14
aridai <jakubkaczma5@gmail.com>
2020-06-05 22:18:26 +0200

    Fixed Ahorn

--- Ahorn/entities/zipMovingPlatform.jl
+++ Ahorn/entities/zipMovingPlatform.jl
@@ -1,8 +1,8 @@
 module JungleHelperZipMovingPlatform
 
-using ..Ahorn
+using ..Ahorn, Maple
 
-@mapdef Entity "JungleHelper/MovingZipPlatform" MovingZipPlatform(x1::Integer, y1::Integer, x2::Integer=x1 + 16, y2::Integer=y1, width::Integer=defaultBlockWidth, texture::String="default") = Entity("movingPlatform", x=x1, y=y1, nodes=Tuple{Int, Int}[(x2, y2)], width=width, texture=texture)
+@pardef MovingZipPlatform(x1::Integer, y1::Integer, x2::Integer=x1 + 16, y2::Integer=y1,width::Integer=defaultBlockWidth, texture::String="default") = Entity("JungleHelper/MovingZipPlatform", x=x1, y=y1, nodes=Tuple{Int, Int}[(x2, y2)], width=width, texture=texture)
 
 
 const placements = Ahorn.PlacementDict(
