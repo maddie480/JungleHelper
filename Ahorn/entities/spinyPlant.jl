106b09dd4010e79221903c7cf835296dc1cce47c
max4805 <max480@free.fr>
2020-06-06 22:41:04 +0200

    Spiny Plants

---
+++ Ahorn/entities/spinyPlant.jl
@@ -0,0 +1,70 @@
+﻿module JungleHelperSpinyPlant
+
+using ..Ahorn, Maple
+
+@mapdef Entity "JungleHelper/SpinyPlant" SpinyPlant(x::Integer, y::Integer, height::Integer=16, color::String="Blue")
+
+colors = ["Blue", "Pink", "Yellow"]
+
+const placements = Ahorn.PlacementDict(
+    "Spiny Plant ($(color)) (Jungle Helper)" => Ahorn.EntityPlacement(
+        SpinyPlant,
+        "rectangle",
+        Dict{String, Any}(
+            "color" => color
+        )
+    ) for color in colors
+)
+
+Ahorn.editingOptions(entity::SpinyPlant) = Dict{String, Any}(
+    "color" => colors
+)
+
+Ahorn.minimumSize(entity::SpinyPlant) = 0, 16
+Ahorn.resizable(entity::SpinyPlant) = false, true
+
+function Ahorn.selection(entity::SpinyPlant)
+    x, y = Ahorn.position(entity)
+    height = Int(get(entity.data, "height", 16))
+
+    return Ahorn.Rectangle(x, y, 24, height)
+end
+
+sections = ["Top", "Mid", "Bottom"]
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::SpinyPlant, room::Maple.Room)
+    color = get(entity.data, "color", "Blue")
+
+    # Values need to be system specific integer
+    x = Int(get(entity.data, "x", 0))
+    y = Int(get(entity.data, "y", 0))
+
+    height = Int(get(entity.data, "height", 16))
+    left = get(entity.data, "left", true)
+    
+    startX = div(x, 8) + 1
+    startY = div(y, 8) + 1
+    stopY = startY + div(height, 8) - 1
+
+    len = stopY - startY
+    for i in 0:2:len
+        if i == len
+            i = len - 1
+        end
+    
+        section = "Mid"
+        if i == 0
+            if get(room.fgTiles.data, (startY - 1, startX), false) == '0'
+                section = "Top"
+            end
+        elseif i == len - 1
+            if get(room.fgTiles.data, (stopY + 1, startX), false) == '0'
+                section = "Bottom"
+            end
+        end
+
+        Ahorn.drawImage(ctx, "JungleHelper/SpinyPlant/Spiny$(color)$(section)", 0, 8 * i)
+    end
+end
+
+end
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Contains the following entities so far:
 - **Bouncy Shroom**: a mushroom Madeline can bounce on! Comes in up, up-left and up-right variants.
 - **Climbable One-Way Platform**: a platform that looks like a sideways jumpthru, except you can go through it. Useful to make f.e. ropes you can climb on.
 - **Theo Statue and Theo Statue Gate**: like a Theo crystal, but twice as tall and ungrabbable. It also does not prevent you from going to the next screen. The Theo statue gate opens when the Theo statue is nearby.
+- **Zip Moving Platform**: a Resort-like moving platform that behaves like a zip mover.
+- **Spiny Plant**: a vertical plant that kills you.
