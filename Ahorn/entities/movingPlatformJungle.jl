279e6f428bcec0f1f0a977a61a69bf0f1fbbb527
max4805 <max480@free.fr>
2020-06-06 18:47:44 +0200

    Code cleanup

--- Ahorn/entities/movingPlatformJungle.jl
+++ Ahorn/entities/movingPlatformJungle.jl
@@ -10,12 +10,12 @@ const placements = Ahorn.PlacementDict(
           "texture" => "Jungle"
         ),
         
-      function(entity)
-        x, y = Int(entity.data["x"]), Int(entity.data["y"])
-        width = Int(get(entity.data, "width", 8))
-        entity.data["x"], entity.data["y"] = x + width, y
-        entity.data["nodes"] = [(x, y)]
-    end
+        function(entity)
+            x, y = Int(entity.data["x"]), Int(entity.data["y"])
+            width = Int(get(entity.data, "width", 8))
+            entity.data["x"], entity.data["y"] = x + width, y
+            entity.data["nodes"] = [(x, y)]
+        end
     )
 )
 
--- Ahorn/entities/zipMovingPlatform.jl
+++ Ahorn/entities/zipMovingPlatform.jl
@@ -7,27 +7,28 @@ using ..Ahorn, Maple
 const placements = Ahorn.PlacementDict()
 
 for texture in Maple.wood_platform_textures
-  placements["Platform (Zip, $(uppercasefirst(texture))) (JungleHelper)"] = Ahorn.EntityPlacement(
-      MovingZipPlatform,
-      "rectangle",
-      Dict{String, Any}(
+    placements["Platform (Zip, $(uppercasefirst(texture))) (JungleHelper)"] = Ahorn.EntityPlacement(
+        MovingZipPlatform,
+        "rectangle",
+        Dict{String, Any}(
           "texture" => texture
-      ),
-      function(entity)
-        x, y = Int(entity.data["x"]), Int(entity.data["y"])
-        width = Int(get(entity.data, "width", 8))
-        entity.data["x"], entity.data["y"] = x + width, y
-        entity.data["nodes"] = [(x, y)]
-    end
-)
+        ),
+        function(entity)
+            x, y = Int(entity.data["x"]), Int(entity.data["y"])
+            width = Int(get(entity.data, "width", 8))
+            entity.data["x"], entity.data["y"] = x + width, y
+            entity.data["nodes"] = [(x, y)]
+        end
+    )
 end
+
 placements["Platform (Zip, Jungle) (JungleHelper)"] = Ahorn.EntityPlacement(
-      MovingZipPlatform,
-      "rectangle",
-      Dict{String, Any}(
-          "texture" => "Jungle"
-      ),
-      function(entity)
+    MovingZipPlatform,
+    "rectangle",
+    Dict{String, Any}(
+        "texture" => "jungle"
+    ),
+    function(entity)
         x, y = Int(entity.data["x"]), Int(entity.data["y"])
         width = Int(get(entity.data, "width", 8))
         entity.data["x"], entity.data["y"] = x + width, y
@@ -42,86 +43,88 @@ Ahorn.minimumSize(entity::MovingZipPlatform) = 8, 0
 Ahorn.editingOptions(entity::MovingZipPlatform) = Dict{String, Any}(
     "texture" => Maple.wood_platform_textures
 )
-function Ahorn.selection(entity::MovingZipPlatform)
-width = Int(get(entity.data, "width", 8))
-startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
-stopX, stopY = Int.(entity.data["nodes"][1])
 
-return [Ahorn.Rectangle(startX, startY, width, 8), Ahorn.Rectangle(stopX, stopY, width, 8)]
+function Ahorn.selection(entity::MovingZipPlatform)
+    width = Int(get(entity.data, "width", 8))
+    startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
+    stopX, stopY = Int.(entity.data["nodes"][1])
+
+    return [Ahorn.Rectangle(startX, startY, width, 8), Ahorn.Rectangle(stopX, stopY, width, 8)]
 end
 
 outerColor = (30, 14, 25) ./ 255
 innerColor = (10, 0, 6) ./ 255
 
 function renderConnection(ctx::Ahorn.Cairo.CairoContext, x::Number, y::Number, nx::Number, ny::Number, width::Number)
-cx, cy = x + floor(Int, width / 2), y + 4
-cnx, cny = nx + floor(Int, width / 2), ny + 4
+    cx, cy = x + floor(Int, width / 2), y + 4
+    cnx, cny = nx + floor(Int, width / 2), ny + 4
 
-length = sqrt((x - nx)^2 + (y - ny)^2)
-theta = atan(cny - cy, cnx - cx)
+    length = sqrt((x - nx)^2 + (y - ny)^2)
+    theta = atan(cny - cy, cnx - cx)
 
-Ahorn.Cairo.save(ctx)
+    Ahorn.Cairo.save(ctx)
 
-Ahorn.translate(ctx, cx, cy)
-Ahorn.rotate(ctx, theta)
+    Ahorn.translate(ctx, cx, cy)
+    Ahorn.rotate(ctx, theta)
 
-Ahorn.setSourceColor(ctx, outerColor)
-Ahorn.set_antialias(ctx, 1)
-Ahorn.set_line_width(ctx, 3);
+    Ahorn.setSourceColor(ctx, outerColor)
+    Ahorn.set_antialias(ctx, 1)
+    Ahorn.set_line_width(ctx, 3);
 
-Ahorn.move_to(ctx, 0, 0)
-Ahorn.line_to(ctx, length, 0)
+    Ahorn.move_to(ctx, 0, 0)
+    Ahorn.line_to(ctx, length, 0)
 
-Ahorn.stroke(ctx)
+    Ahorn.stroke(ctx)
 
-Ahorn.setSourceColor(ctx, innerColor)
-Ahorn.set_antialias(ctx, 1)
-Ahorn.set_line_width(ctx, 1);
+    Ahorn.setSourceColor(ctx, innerColor)
+    Ahorn.set_antialias(ctx, 1)
+    Ahorn.set_line_width(ctx, 1);
 
-Ahorn.move_to(ctx, 0, 0)
-Ahorn.line_to(ctx, length, 0)
+    Ahorn.move_to(ctx, 0, 0)
+    Ahorn.line_to(ctx, length, 0)
 
-Ahorn.stroke(ctx)
+    Ahorn.stroke(ctx)
 
-Ahorn.Cairo.restore(ctx)
+    Ahorn.Cairo.restore(ctx)
 end
 
 function renderPlatform(ctx::Ahorn.Cairo.CairoContext, texture::String, x::Number, y::Number, width::Number)
-tilesWidth = div(width, 8)
+    tilesWidth = div(width, 8)
 
-for i in 2:tilesWidth - 1
-  Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + 8 * (i - 1), y, 8, 0, 8, 8)
-end
+    for i in 2:tilesWidth - 1
+      Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + 8 * (i - 1), y, 8, 0, 8, 8)
+    end
 
-Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x, y, 0, 0, 8, 8)
-Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + tilesWidth * 8 - 8, y, 24, 0, 8, 8)
-Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + floor(Int, width / 2) - 4, y, 16, 0, 8, 8)
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x, y, 0, 0, 8, 8)
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + tilesWidth * 8 - 8, y, 24, 0, 8, 8)
+    Ahorn.drawImage(ctx, "objects/woodPlatform/$texture", x + floor(Int, width / 2) - 4, y, 16, 0, 8, 8)
 end
 
 function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::MovingZipPlatform, room::Maple.Room)
-width = Int(get(entity.data, "width", 8))
+    width = Int(get(entity.data, "width", 8))
 
-x, y = Int(entity.data["x"]), Int(entity.data["y"])
-nx, ny = Int.(entity.data["nodes"][1])
+    x, y = Int(entity.data["x"]), Int(entity.data["y"])
+    nx, ny = Int.(entity.data["nodes"][1])
 
 
-texture = get(entity.data, "texture", "default")
+    texture = get(entity.data, "texture", "default")
 
-renderConnection(ctx, x, y, nx, ny, width)
-renderPlatform(ctx, texture, x, y, width)
+    renderConnection(ctx, x, y, nx, ny, width)
+    renderPlatform(ctx, texture, x, y, width)
 end
 
 function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::MovingZipPlatform, room::Maple.Room)
-width = Int(get(entity.data, "width", 8))
+    width = Int(get(entity.data, "width", 8))
 
-startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
-stopX, stopY = Int.(entity.data["nodes"][1])
+    startX, startY = Int(entity.data["x"]), Int(entity.data["y"])
+    stopX, stopY = Int.(entity.data["nodes"][1])
 
-texture = get(entity.data, "texture", "default")
+    texture = get(entity.data, "texture", "default")
 
-renderPlatform(ctx, texture, startX, startY, width)
-renderPlatform(ctx, texture, stopX, stopY, width)
+    renderPlatform(ctx, texture, startX, startY, width)
+    renderPlatform(ctx, texture, stopX, stopY, width)
 
-Ahorn.drawArrow(ctx, startX + width / 2, startY, stopX + width / 2, stopY, Ahorn.colors.selection_selected_fc, headLength=6)
+    Ahorn.drawArrow(ctx, startX + width / 2, startY, stopX + width / 2, stopY, Ahorn.colors.selection_selected_fc, headLength=6)
 end
+
 end
