8f60b58d71a16064acaf164d6ff59ab34072f081
aridai <jakubkaczma5@gmail.com>
2020-06-06 16:26:44 +0200

    Fixed bug with Moving Platform (Jungle) placement

--- Ahorn/entities/movingPlatformJungle.jl
+++ Ahorn/entities/movingPlatformJungle.jl
@@ -8,7 +8,14 @@ const placements = Ahorn.PlacementDict(
         "rectangle",
         Dict{String, Any}(
           "texture" => "Jungle"
-        )
+        ),
+        
+      function(entity)
+        x, y = Int(entity.data["x"]), Int(entity.data["y"])
+        width = Int(get(entity.data, "width", 8))
+        entity.data["x"], entity.data["y"] = x + width, y
+        entity.data["nodes"] = [(x, y)]
+    end
     )
 )
 
