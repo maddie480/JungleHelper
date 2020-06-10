0429457e0950d1bf57e4918150064754306b0d67
max4805 <max480@free.fr>
2020-06-10 16:05:22 +0200

    First version of the Rolling Rock
    ... more like Scrolling Circle of Doom if you ask me.

---
+++ Code/CircleColliderWithRectangles.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.JungleHelper {
+    /// <summary>
+    /// Circle collider, except using multiple rectangles instead, allowing them to collide with tiles for example.
+    /// </summary>
+    class CircleColliderWithRectangles : ColliderList {
+        private int radius;
+        private float x;
+        private float y;
+
+        public CircleColliderWithRectangles(int radius, float x = 0f, float y = 0f) : base() {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+
+            for (int i = -radius; i < radius; i++) {
+                // I wasn't missing trigonometry hh
+                float chunkWidth = (float) Math.Abs(Math.Cos(Math.Asin((double) i / radius)) * radius);
+                Add(new Hitbox(chunkWidth * 2, 1, -chunkWidth + x, i + y));
+            }
+        }
+
+        public override void Render(Camera camera, Color color) {
+            Draw.Circle(AbsolutePosition + new Vector2(x, y), radius, color, 4);
+        }
+    }
+}
