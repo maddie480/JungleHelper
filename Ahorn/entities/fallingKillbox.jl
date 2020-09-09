2dd8d5bb44de46ceacc0d1290adce56369b6c46d
max4805 <max480@free.fr>
2020-09-09 15:04:28 +0200

    Falling Killbox
    Like a killbox, except falling. :theoreticalwoke:

---
+++ Ahorn/entities/fallingKillbox.jl
@@ -0,0 +1,33 @@
+module JungleHelperFallingKillbox
+
+using ..Ahorn, Maple
+
+@mapdef Entity "JungleHelper/FallingKillbox" FallingKillbox(x::Integer, y::Integer, width::Integer=Maple.defaultBlockWidth, fallSpeed::Number=100.0)
+    
+const placements = Ahorn.PlacementDict(
+    "Falling Killbox (Jungle Helper)" => Ahorn.EntityPlacement(
+        FallingKillbox,
+        "rectangle"
+    ),
+)
+
+Ahorn.minimumSize(entity::FallingKillbox) = 8, 0
+Ahorn.resizable(entity::FallingKillbox) = true, false
+
+function Ahorn.selection(entity::FallingKillbox)
+    x, y = Ahorn.position(entity)
+
+    width = Int(get(entity.data, "width", 8))
+    height = 32
+
+    return Ahorn.Rectangle(x, y, width, height)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::FallingKillbox, room::Maple.Room)
+    width = Int(get(entity.data, "width", 32))
+    height = 32
+
+    Ahorn.drawRectangle(ctx, 0, 0, width, height, (0.8, 0.4, 0.4, 0.8), (0.0, 0.0, 0.0, 0.0))
+end
+
+end
---
+++ Code/Entities/FallingKillbox.cs
@@ -0,0 +1,25 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.JungleHelper.Entities {
+    /// <summary>
+    /// A killbox, except it's falling. :wow:
+    /// </summary>
+    [CustomEntity("JungleHelper/FallingKillbox")]
+    [TrackedAs(typeof(Killbox))]
+    class FallingKillbox : Killbox {
+        private readonly float fallingSpeed;
+
+        public FallingKillbox(EntityData data, Vector2 offset) : base(data, offset) {
+            fallingSpeed = data.Float("fallingSpeed", 100f);
+        }
+
+        public override void Update() {
+            base.Update();
+
+            // fall
+            Position.Y += fallingSpeed * Engine.DeltaTime;
+        }
+    }
+}
