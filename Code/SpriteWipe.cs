ceefff51aa2022f1faea29105f252d5598023d9d
max4805 <max480@free.fr>
2020-10-21 22:53:37 +0200

    Sprite Wipe: load wipe textures only when entering a map using them

--- Code/SpriteWipe.cs
+++ Code/SpriteWipe.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Monocle;
+using MonoMod.Utils;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Celeste.Mod.JungleHelper {
     // Useful when you want to do a wipe, but you know how to sprite better than you know how to code.
@@ -10,27 +12,73 @@ namespace Celeste.Mod.JungleHelper {
     class SpriteWipe : ScreenWipe {
         public static void Load() {
             On.Celeste.Mod.Meta.MapMeta.ApplyTo += onParseScreenWipe;
+            On.Celeste.LevelLoader.LoadingThread += onLevelLoad;
         }
 
         public static void Unload() {
             On.Celeste.Mod.Meta.MapMeta.ApplyTo -= onParseScreenWipe;
+            On.Celeste.LevelLoader.LoadingThread -= onLevelLoad;
         }
+
+        private static Atlas wipeAtlas;
+        private static string atlasPath;
 
         private static void onParseScreenWipe(On.Celeste.Mod.Meta.MapMeta.orig_ApplyTo orig, Meta.MapMeta self, AreaData area) {
             orig(self, area);
 
             if (!string.IsNullOrEmpty(self.Wipe) && self.Wipe.StartsWith("JungleHelper/SpriteWipe:")) {
-                string spritePath = self.Wipe.Substring("JungleHelper/SpriteWipe:".Length);
-                area.Wipe = (scene, wipeIn, onComplete) => new SpriteWipe(scene, wipeIn, spritePath, onComplete);
+                string spriteName = self.Wipe.Substring("JungleHelper/SpriteWipe:".Length);
+                area.Wipe = (scene, wipeIn, onComplete) => {
+                    if (spriteName == atlasPath) {
+                        // the sprites are loaded in, proceed
+                        new SpriteWipe(scene, wipeIn, onComplete);
+                    } else {
+                        // I'm sure no-one will notice, right?
+                        new AngledWipe(scene, wipeIn, onComplete);
+                    }
+                };
+
+                // let's make sure the wipe is in map metadata because this can get weird.
+                if (area.GetMeta() != null) {
+                    area.GetMeta().Wipe = self.Wipe;
+                }
+            }
+        }
+
+        private static void onLevelLoad(On.Celeste.LevelLoader.orig_LoadingThread orig, LevelLoader self) {
+            AreaKey area = new DynData<LevelLoader>(self).Get<Session>("session").Area;
+            string wipe = AreaData.Get(area)?.GetMeta()?.Wipe;
+            if (wipe != null && wipe.StartsWith("JungleHelper/SpriteWipe:")) {
+                string spriteName = wipe.Substring("JungleHelper/SpriteWipe:".Length);
+                loadSpritesFor(spriteName);
+            }
+
+            orig(self);
+        }
+
+        private static void loadSpritesFor(string spritePath) {
+            if (atlasPath != spritePath) {
+                Stopwatch timer = Stopwatch.StartNew();
+
+                if (wipeAtlas != null) {
+                    Logger.Log(LogLevel.Info, "JungleHelper/SpriteWipe", $"Unloading sprites for wipe {atlasPath}");
+                    wipeAtlas.Dispose();
+                }
+
+                wipeAtlas = Atlas.FromAtlas("Graphics/Atlases/Wipes/" + spritePath, Atlas.AtlasDataFormat.PackerNoAtlas);
+                atlasPath = spritePath;
+
+                timer.Stop();
+                Logger.Log(LogLevel.Info, "JungleHelper/SpriteWipe", $"Loading sprites for {spritePath} took {timer.ElapsedMilliseconds} ms");
             }
         }
 
         private readonly List<MTexture> spritesIn;
         private readonly List<MTexture> spritesOut;
 
-        public SpriteWipe(Scene scene, bool wipeIn, string spritePath, Action onComplete = null) : base(scene, wipeIn, onComplete) {
-            spritesIn = GFX.Gui.GetAtlasSubtextures(spritePath + "/wipein");
-            spritesOut = GFX.Gui.GetAtlasSubtextures(spritePath + "/wipeout");
+        public SpriteWipe(Scene scene, bool wipeIn, Action onComplete = null) : base(scene, wipeIn, onComplete) {
+            spritesIn = wipeAtlas.GetAtlasSubtextures("wipein");
+            spritesOut = wipeAtlas.GetAtlasSubtextures("wipeout");
         }
 
         public override void Render(Scene scene) {
