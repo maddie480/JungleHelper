19a467bda33f8ccf22a256bca30fbe54902d5b63
max4805 <max480@free.fr>
2020-11-10 12:09:56 +0100

    Create an Into The Jungle-specific code mod that hides rooms ending with _HideInMap

---
+++ Z_IntoTheJungleCodeMod/IntoTheJungleCodeModule.cs
@@ -0,0 +1,31 @@
+﻿using Celeste.Editor;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Celeste.Mod.IntoTheJungleCodeMod {
+    public class IntoTheJungleCodeModule : EverestModule {
+        private static FieldInfo mapEditorLevelList = typeof(MapEditor).GetField("levels", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public override void Load() {
+            On.Celeste.Editor.MapEditor.ctor += modHideMap;
+        }
+
+        public override void Unload() {
+            On.Celeste.Editor.MapEditor.ctor -= modHideMap;
+        }
+
+        private static void modHideMap(On.Celeste.Editor.MapEditor.orig_ctor orig, MapEditor self, AreaKey area, bool reloadMapData) {
+            orig(self, area, reloadMapData);
+
+            if (area.GetLevelSet() == "Into The Jungle") {
+                List<LevelTemplate> mapList = (List<LevelTemplate>) mapEditorLevelList.GetValue(self);
+                for (int i = mapList.Count - 1; i >= 0; i--) {
+                    // Hide all maps which names end with _HideInMap
+                    if (mapList[i].Name.EndsWith("_HideInMap")) {
+                        mapList.Remove(mapList[i]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Z_IntoTheJungleCodeMod/README.md
@@ -0,0 +1,3 @@
+# Into the Jungle Code Mod
+
+This is a small project containing some specific code for the Into the Jungle map.
