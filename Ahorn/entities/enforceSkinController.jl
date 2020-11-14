cd2c85a5d57dbd7775d1908385eeaa3d0d8197bd
max4805 <max480@free.fr>
2020-11-14 23:10:33 +0100

    Added Ahorn plugin for Enforce Skin Controller

---
+++ Ahorn/entities/enforceSkinController.jl
@@ -0,0 +1,21 @@
+﻿module JungleHelperEnforceSkinController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "JungleHelper/EnforceSkinController" EnforceSkinController(x::Integer, y::Integer, showPostcard::Bool=true)
+
+const placements = Ahorn.PlacementDict(
+    "Enforce Skin Controller (Jungle Helper)" => Ahorn.EntityPlacement(
+        EnforceSkinController
+    )
+)
+
+function Ahorn.selection(entity::EnforceSkinController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::EnforceSkinController, room::Maple.Room) = Ahorn.drawImage(ctx, "ahorn/JungleHelper/enforce_skin_controller", -12, -12)
+
+end
