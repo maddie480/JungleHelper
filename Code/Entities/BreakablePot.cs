12a94ab65439c727f1482ea28e92b6a95d9b880c
max4805 <max480@free.fr>
2021-01-14 23:26:49 +0100

    [ci skip] added comments and removed unused imports in breakable pot

--- Code/Entities/BreakablePot.cs
+++ Code/Entities/BreakablePot.cs
@@ -3,10 +3,6 @@ using Microsoft.Xna.Framework;
 using Monocle;
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Celeste.Mod.JungleHelper.Entities {
     [CustomEntity("JungleHelper/BreakablePot")]
@@ -18,12 +14,16 @@ namespace Celeste.Mod.JungleHelper.Entities {
         private Vector2 speed;
 
         public BreakablePot(EntityData data, Vector2 offset) : base(data.Position + offset) {
+            // set up the rupee first, so that it is behind the pot.
             Add(rupee = new Image(GFX.Game[data.Attr("rupeeImage", "JungleHelper/Breakable Pot/rupee")]));
             rupee.CenterOrigin();
             rupee.Y = -8;
             rupee.X = 1;
+
+            // set up the pot sprite.
             Add(sprite = JungleHelperModule.CreateReskinnableSprite(data, "breakable_pot"));
 
+            // make the pot holdable.
             Add(hold = new Holdable());
             hold.PickupCollider = new Hitbox(17f, 20f, -8f, -20f);
             hold.SlowRun = false;
@@ -62,6 +62,8 @@ namespace Celeste.Mod.JungleHelper.Entities {
                 }
                 speed.Y = Calc.Approach(speed.Y, 200f, acceleration * Engine.DeltaTime);
             }
+
+            // move the pot around, breaking it if it hits anything.
             MoveV(speed.Y * Engine.DeltaTime, _ => breakPot());
             MoveH(speed.X * Engine.DeltaTime, _ => breakPot());
         }
@@ -80,6 +82,7 @@ namespace Celeste.Mod.JungleHelper.Entities {
         }
 
         private IEnumerator animateRupeeRoutine() {
+            // rupee goes up
             float p = 0f;
             while (p < 1f) {
                 rupee.Y = -5 - Ease.CubeOut(p) * 20f;
@@ -88,8 +91,10 @@ namespace Celeste.Mod.JungleHelper.Entities {
             }
             rupee.Y = -25f;
 
+            // wait for a bit
             yield return 0.1f;
 
+            // rupee fades out
             float a = 1f;
             while (a > 0f) {
                 rupee.Color = Color.White * a;
@@ -97,6 +102,7 @@ namespace Celeste.Mod.JungleHelper.Entities {
                 a -= 4f * Engine.DeltaTime;
             }
 
+            // entity is now invisible so it can go away.
             RemoveSelf();
         }
     }
