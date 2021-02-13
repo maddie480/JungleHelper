bdc17877c0d09544ad24dd4e7fef3e410c527409
max4805 <max480@free.fr>
2021-02-14 00:18:32 +0100

    Added new sprite wipes (Arrow, Rocks, Spinners, Dots)

--- Ahorn/libraries/jungleHelperWipeDropdown.jl
+++ Ahorn/libraries/jungleHelperWipeDropdown.jl
@@ -3,6 +3,10 @@
 using ..Ahorn
 
 # Inject Jungle Helper wipes into the dropdown in map metadata.
-Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Vines (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Ch3Vine"
+Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Arrow (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Arrow"
+Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Dots (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Dots"
+Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Rocks (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Rocks"
+Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Spinners (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Speen"
+Ahorn.MetadataWindow.metaDropdownOptions["Wipe"]["Vines (Jungle Helper)"] = "JungleHelper/SpriteWipe:JungleHelper/Vines"
 
 end
