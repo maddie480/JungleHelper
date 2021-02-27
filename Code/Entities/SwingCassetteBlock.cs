2b0fec860a1295f00c00a518c1cd65f5162649d8
max4805 <max480@free.fr>
2021-02-27 19:11:58 +0100

    Fixed swing cassette blocks being inactive if no vanilla cassette block is in the room

--- Code/Entities/SwingCassetteBlock.cs
+++ Code/Entities/SwingCassetteBlock.cs
@@ -3,20 +3,53 @@ using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using Monocle;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using System;
 using System.Reflection;
 
 namespace Celeste.Mod.JungleHelper.Entities {
-    [CustomEntity("JungleHelper/SwingCassetteBlock")]
     [Tracked]
     [TrackedAs(typeof(CassetteBlock))]
     class SwingCassetteBlock : CassetteBlock {
+        private static ILHook loadLevelILHook;
+        public static bool IsSwingCassetteBlock = false;
+
         public static void Load() {
+            loadLevelILHook = new ILHook(typeof(Level).GetMethod("orig_LoadLevel"), hookLoadLevel);
             IL.Celeste.CassetteBlockManager.AdvanceMusic += swingCassetteMusic;
         }
 
         public static void Unload() {
+            loadLevelILHook?.Dispose();
+            loadLevelILHook = null;
+
             IL.Celeste.CassetteBlockManager.AdvanceMusic -= swingCassetteMusic;
+        }
+
+        private static void hookLoadLevel(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            if (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<EntityData>("Name"))) {
+                Logger.Log("JungleHelper/SwingCassetteBlock", $"Modding orig_LoadLevel at {cursor.Index} to recognize Swing Cassette Blocks");
+
+                // JungleHelper/SwingCassetteBlock should be replaced with cassetteBlock to get the same behavior as cassette blocks.
+                cursor.EmitDelegate<Func<string, string>>(orig => {
+                    IsSwingCassetteBlock = (orig == "JungleHelper/SwingCassetteBlock");
+                    if (IsSwingCassetteBlock) {
+                        return "cassetteBlock";
+                    }
+                    return orig;
+                });
+            }
+
+            if (cursor.TryGotoNext(instr => instr.MatchNewobj<CassetteBlock>())) {
+                Logger.Log("JungleHelper/SwingCassetteBlock", $"Modding orig_LoadLevel at {cursor.Index} to instantiate Swing Cassette Blocks");
+
+                cursor.Emit(OpCodes.Ldsfld, typeof(SwingCassetteBlock).GetField("IsSwingCassetteBlock"));
+                cursor.Emit(OpCodes.Brfalse, cursor.Next); // jump over the instruction we are going to insert.
+                cursor.Emit(OpCodes.Newobj, typeof(SwingCassetteBlock).GetConstructor(new Type[] { typeof(EntityData), typeof(Vector2), typeof(EntityID) }));
+                cursor.Emit(OpCodes.Br, cursor.Next.Next); // jump over the instruction building a vanilla cassette block.
+            }
         }
 
         private static void swingCassetteMusic(ILContext il) {
