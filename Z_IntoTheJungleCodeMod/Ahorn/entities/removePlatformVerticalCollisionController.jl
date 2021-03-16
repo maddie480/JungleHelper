90bc0e41b3df81a25e080ac25c85d4e584ed2391
max4805 <max480@free.fr>
2021-03-16 20:06:24 +0100

    [IntoTheJungleCodeMod] Added a cursed controller disabling vertical collision between a moving solid and other solids

---
+++ Z_IntoTheJungleCodeMod/Ahorn/entities/removePlatformVerticalCollisionController.jl
@@ -0,0 +1,21 @@
+﻿module IntoTheJungleCodeModRemovePlatformVerticalCollisionController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController" RemovePlatformVerticalCollisionController(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Remove Platform Vertical Collision Controller\n(Into The Jungle code mod)" => Ahorn.EntityPlacement(
+        RemovePlatformVerticalCollisionController
+    )
+)
+
+function Ahorn.selection(entity::RemovePlatformVerticalCollisionController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::RemovePlatformVerticalCollisionController, room::Maple.Room) = Ahorn.drawImage(ctx, Ahorn.Assets.northernLights, -12, -12)
+
+end
---
+++ Z_IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController.cs
@@ -0,0 +1,39 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.IntoTheJungleCodeMod {
+    // this cursed controller allows to boost the performance of that room with a bunch of falling blocks that won't ever hit anything anyway,
+    // by just removing the collide check between a moving solid and other solids.
+    [CustomEntity("IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController")]
+    class RemovePlatformVerticalCollisionController : Entity {
+        public RemovePlatformVerticalCollisionController(EntityData data, Vector2 offset) : base(data.Position + offset) { }
+
+        public override void Awake(Scene scene) {
+            base.Awake(scene);
+
+            Logger.Log("IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController", "Removing platform vertical collision to boost performance");
+            On.Celeste.Platform.MoveVCollideSolids += moveVButDontCollideSolids;
+        }
+
+        public override void Removed(Scene scene) {
+            base.Removed(scene);
+
+            Logger.Log("IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController", "Restoring platform vertical collision");
+            On.Celeste.Platform.MoveVCollideSolids -= moveVButDontCollideSolids;
+        }
+
+        public override void SceneEnd(Scene scene) {
+            base.SceneEnd(scene);
+
+            Logger.Log("IntoTheJungleCodeMod/RemovePlatformVerticalCollisionController", "Restoring platform vertical collision");
+            On.Celeste.Platform.MoveVCollideSolids -= moveVButDontCollideSolids;
+        }
+
+        private bool moveVButDontCollideSolids(On.Celeste.Platform.orig_MoveVCollideSolids orig, Platform self, float moveV, bool thruDashBlocks, Action<Vector2, Vector2, Platform> onCollide) {
+            self.MoveV(moveV);
+            return false; // no collision
+        }
+    }
+}
