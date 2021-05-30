b00a08267c30b025bad8498745e7f64b4cd594ab
max4805 <max480@free.fr>
2021-05-30 23:30:47 +0200

    [IntoTheJungleCodeMod] Prevent player from seeing more than 1 ending per minute

--- Z_IntoTheJungleCodeMod/EndingDialogue.cs
+++ Z_IntoTheJungleCodeMod/EndingDialogue.cs
@@ -8,6 +8,9 @@ namespace Celeste.Mod.IntoTheJungleCodeMod {
     class EndingDialogue : CutsceneEntity {
         private SoundSource phoneSfx;
 
+        private static string lastEnding;
+        private static long lastEndingTimestamp = long.MinValue;
+
         public EndingDialogue() : base(fadeInOnSkip: false, endingChapterAfter: true) { }
 
         public override void OnBegin(Level level) {
@@ -54,115 +57,125 @@ namespace Celeste.Mod.IntoTheJungleCodeMod {
 
             string dialogue;
 
-            // draw a number between 0 and 1
-            double rng = new Random().NextDouble();
-            if (IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter > 50) {
-                // more than 50 endings without a rare one => force a rare ending (0.98 to 1)
-                rng = (rng / 50) + 0.98;
-            }
+            if (Environment.TickCount - lastEndingTimestamp > 60000) {
+                // draw a number between 0 and 1
+                double rng = new Random().NextDouble();
+                if (IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter > 50) {
+                    // more than 50 endings without a rare one => force a rare ending (0.98 to 1)
+                    rng = (rng / 50) + 0.98;
+                }
 
-            LevelSetStats stats = SaveData.Instance.GetLevelSetStats();
-            double redBerryPercent = stats.TotalStrawberries / (double) stats.MaxStrawberries;
+                LevelSetStats stats = SaveData.Instance.GetLevelSetStats();
+                double redBerryPercent = stats.TotalStrawberries / (double) stats.MaxStrawberries;
 
-            if (SaveData.Instance.Time < TimeSpan.FromMinutes(30).Ticks && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Speedrun_Fast")) {
-                // Finishing the A-sides with a game file under 30 minutes For the first time.
-                dialogue = "JungleEnd_Speedrun_Fast";
+                if (SaveData.Instance.Time < TimeSpan.FromMinutes(30).Ticks && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Speedrun_Fast")) {
+                    // Finishing the A-sides with a game file under 30 minutes For the first time.
+                    dialogue = "JungleEnd_Speedrun_Fast";
 
-                // blacklist both speedrun endings
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Speedrun_Fast");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Speedrun_Normal");
+                    // blacklist both speedrun endings
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Speedrun_Fast");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Speedrun_Normal");
 
-            } else if (SaveData.Instance.Time < TimeSpan.FromHours(1).Ticks && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Speedrun_Normal")) {
-                // Finishing the A-sides with a game file under 1 hour for the first time.
-                dialogue = "JungleEnd_Speedrun_Normal";
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
+                } else if (SaveData.Instance.Time < TimeSpan.FromHours(1).Ticks && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Speedrun_Normal")) {
+                    // Finishing the A-sides with a game file under 1 hour for the first time.
+                    dialogue = "JungleEnd_Speedrun_Normal";
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
 
-            } else if (stats.TotalGoldenStrawberries >= 15 /* intentionally hardcoded */ && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_A")) {
-                // After collecting all 10 golden berries the FIRST time and then completing 5A (not counting grabless 1a)
-                dialogue = "JungleEnd_All_Golden_A";
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
+                } else if (stats.TotalGoldenStrawberries >= 15 /* intentionally hardcoded */ && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_A")) {
+                    // After collecting all 10 golden berries the FIRST time and then completing 5A (not counting grabless 1a)
+                    dialogue = "JungleEnd_All_Golden_A";
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
 
-            } else if (IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_A") && IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Red")
-                && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_B")) {
-                // Will happen once after getting both JungleEnd_All_Golden_A AND JungleEnd_All_Red to display and then completing 5A again.
-                dialogue = "JungleEnd_All_Golden_B";
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
+                } else if (IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_A") && IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Red")
+                    && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Golden_B")) {
+                    // Will happen once after getting both JungleEnd_All_Golden_A AND JungleEnd_All_Red to display and then completing 5A again.
+                    dialogue = "JungleEnd_All_Golden_B";
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
 
-            } else if (!IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_First_Run")) {
-                // First playthrough on a game file. 
-                dialogue = "JungleEnd_First_Run";
+                } else if (!IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_First_Run")) {
+                    // First playthrough on a game file. 
+                    dialogue = "JungleEnd_First_Run";
 
-            } else if (redBerryPercent >= 1 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Red")) {
-                // After collecting all XX Red berries for the first time
-                dialogue = "JungleEnd_All_Red";
+                } else if (redBerryPercent >= 1 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_All_Red")) {
+                    // After collecting all XX Red berries for the first time
+                    dialogue = "JungleEnd_All_Red";
 
-                // ban all worse berry endings
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_All_Red");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_High");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
+                    // ban all worse berry endings
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_All_Red");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_High");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
 
-            } else if (redBerryPercent >= 0.75 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_High")) {
-                // Collecting 75% of berries for the first time
-                dialogue = "JungleEnd_Berry_High";
+                } else if (redBerryPercent >= 0.75 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_High")) {
+                    // Collecting 75% of berries for the first time
+                    dialogue = "JungleEnd_Berry_High";
 
-                // ban all worse berry endings
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_High");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
+                    // ban all worse berry endings
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_High");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
 
-            } else if (redBerryPercent >= 0.5 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_Mid")) {
-                // Collecting 50% of berries for the first time
-                dialogue = "JungleEnd_Berry_Mid";
+                } else if (redBerryPercent >= 0.5 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_Mid")) {
+                    // Collecting 50% of berries for the first time
+                    dialogue = "JungleEnd_Berry_Mid";
 
-                // ban all worse berry endings
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
+                    // ban all worse berry endings
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Mid");
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_Berry_Low");
 
-            } else if (redBerryPercent >= 0.25 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_Low")) {
-                // Collecting 25% of berries for the first time
-                dialogue = "JungleEnd_Berry_Low";
-                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
+                } else if (redBerryPercent >= 0.25 && !IntoTheJungleCodeModule.EndingDialogueBlacklist.Contains("JungleEnd_Berry_Low")) {
+                    // Collecting 25% of berries for the first time
+                    dialogue = "JungleEnd_Berry_Low";
+                    IntoTheJungleCodeModule.EndingDialogueBlacklist.Add(dialogue);
 
-            } else if (rng < 0.5) {
-                // 50% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_A";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
+                } else if (rng < 0.5) {
+                    // 50% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_A";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
 
-            } else if (rng < 0.9) {
-                // 40% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_B";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
+                } else if (rng < 0.9) {
+                    // 40% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_B";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
 
-            } else if (rng < 0.98) {
-                // 8% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_C";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
+                } else if (rng < 0.98) {
+                    // 8% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_C";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter++;
 
-            } else if (rng < 0.985) {
-                // 0.5% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_D";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
+                } else if (rng < 0.985) {
+                    // 0.5% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_D";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
 
-            } else if (rng < 0.99) {
-                // 0.5% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_E";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
+                } else if (rng < 0.99) {
+                    // 0.5% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_E";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
 
-            } else if (rng < 0.995) {
-                // 0.5% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_F";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
+                } else if (rng < 0.995) {
+                    // 0.5% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_F";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
 
+                } else {
+                    // 0.5% chance if no other endings trigger.
+                    dialogue = "JungleEnd_Percent_G";
+                    IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
+
+                }
+
+                // whatever the dialogue is, the first playthrough is now done.
+                IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_First_Run");
+
+                // save the dialogue we picked in memory to prevent the player from teleporting repeatedly to see all endings.
+                lastEnding = dialogue;
+                lastEndingTimestamp = Environment.TickCount;
             } else {
-                // 0.5% chance if no other endings trigger.
-                dialogue = "JungleEnd_Percent_G";
-                IntoTheJungleCodeModule.ModSaveData.NoRareEndingCounter = 0;
-
+                // the player last reached the ending dialogue less than a minute ago!
+                // replay the same dialogue.
+                dialogue = lastEnding;
             }
-
-            // whatever the dialogue is, the first playthrough is now done.
-            IntoTheJungleCodeModule.EndingDialogueBlacklist.Add("JungleEnd_First_Run");
 
             // change music
             Level level = SceneAs<Level>();
