1d27110743d6e6536e20e9e8994f3677ddc48d74
max4805 <max480@free.fr>
2021-06-18 15:46:19 +0200

    Added Cheat Code Controller

---
+++ Ahorn/entities/cheatCodeController.jl
@@ -0,0 +1,21 @@
+﻿module JungleHelperCheatCodeController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "JungleHelper/CheatCodeController" CheatCodeController(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Cheat Code Controller (Jungle Helper)" => Ahorn.EntityPlacement(
+        CheatCodeController
+    )
+)
+
+function Ahorn.selection(entity::CheatCodeController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::CheatCodeController, room::Maple.Room) = Ahorn.drawImage(ctx, "ahorn/JungleHelper/cheat_code", -12, -12)
+
+end
---
+++ Code/Entities/CheatCodeController.cs
@@ -0,0 +1,25 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.JungleHelper.Entities {
+    [CustomEntity("JungleHelper/CheatCodeController")]
+    static class CheatCodeController {
+        private class Nothing : Entity {
+            public override void Added(Scene scene) {
+                base.Added(scene);
+                RemoveSelf();
+            }
+        }
+
+        public static Entity Load(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            if (!SaveData.Instance.CheatMode) {
+                // yes this is definitely a very original entity I made
+                return new UnlockEverythingThingy();
+            }
+
+            // we don't want the controller to spawn, but we don't want a "failed loading entity" warning either.
+            return new Nothing();
+        }
+    }
+}
