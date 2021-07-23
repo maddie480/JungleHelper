ab4e666e0146f0b10d2886bc9a13cce1fd3a9ae7
max4805 <max480@free.fr>
2021-07-23 22:44:45 +0200

    [IntoTheJungleCodeMod] Added overly specific set color grade if flag on spawn controller for ch5

---
+++ Z_IntoTheJungleCodeMod/Ahorn/entities/setColorGradeIfFlagOnSpawnController.jl
@@ -0,0 +1,21 @@
+﻿module IntoTheJungleCodeModSetColorGradeIfFlagOnSpawnController
+
+using ..Ahorn, Maple
+
+@mapdef Entity "IntoTheJungleCodeMod/SetColorGradeIfFlagOnSpawnController" SetColorGradeIfFlagOnSpawnController(x::Integer, y::Integer, flag::String="", colorGrade::String="")
+
+const placements = Ahorn.PlacementDict(
+    "Set Color Grade If Flag On Spawn Controller\n(Into The Jungle code mod)" => Ahorn.EntityPlacement(
+        SetColorGradeIfFlagOnSpawnController
+    )
+)
+
+function Ahorn.selection(entity::SetColorGradeIfFlagOnSpawnController)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.Rectangle(x - 12, y - 12, 24, 24)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::SetColorGradeIfFlagOnSpawnController, room::Maple.Room) = Ahorn.drawImage(ctx, Ahorn.Assets.northernLights, -12, -12)
+
+end
---
+++ Z_IntoTheJungleCodeMod/SetColorGradeIfFlagOnSpawnController.cs
@@ -0,0 +1,30 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.IntoTheJungleCodeMod {
+    // ... yeah this name is kind of a meme.
+    [CustomEntity("IntoTheJungleCodeMod/SetColorGradeIfFlagOnSpawnController")]
+    class SetColorGradeIfFlagOnSpawnController : Entity {
+        private readonly string colorGrade;
+        private readonly string flag;
+
+        public SetColorGradeIfFlagOnSpawnController(EntityData data, Vector2 offset) {
+            colorGrade = data.Attr("colorGrade");
+            flag = data.Attr("flag");
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            // "if flag on spawn"
+            if (SceneAs<Level>().Session.GetFlag(flag)) {
+                // "set color grade"
+                SceneAs<Level>().SnapColorGrade(colorGrade);
+            }
+
+            // then we're done
+            RemoveSelf();
+        }
+    }
+}
