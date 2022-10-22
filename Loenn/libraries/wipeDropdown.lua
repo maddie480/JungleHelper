37e1c8e3ca2248a140fe19b50f5fac2f0f81ece8
max4805 <max480@free.fr>
2022-10-22 20:29:18 +0200

    Add Jungle Helper wipes to Loenn

---
+++ Loenn/libraries/wipeDropdown.lua
@@ -0,0 +1,10 @@
+﻿local celesteEnums = require("consts.celeste_enums")
+
+-- Inject Jungle Helper wipes into the dropdown in map metadata.
+celesteEnums.wipe_names["Arrow [Jungle Helper]"] = "MaxHelpingHand/CustomWipe:JungleHelper/Arrow"
+celesteEnums.wipe_names["Dots [Jungle Helper]"] = "MaxHelpingHand/CustomWipe:JungleHelper/Dots"
+celesteEnums.wipe_names["Rocks [Jungle Helper]"] = "MaxHelpingHand/CustomWipe:JungleHelper/Rocks"
+celesteEnums.wipe_names["Spinners [Jungle Helper]"] = "MaxHelpingHand/CustomWipe:JungleHelper/Speen"
+celesteEnums.wipe_names["Vines [Jungle Helper]"] = "MaxHelpingHand/CustomWipe:JungleHelper/Vines"
+
+return {}
